a754ca4d4e68c2a5d8d3c7ced2c6f10656f6aeee
Micah Silverman <micah@afitnerd.com>
2021-05-06 19:21:35 -0400

    wire up split

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
-const port = process.env.PORT || 8000;
+const port = process.env.POST || 8000;
 const app = express();
 
+var SplitFactory = require('@splitsoftware/splitio').SplitFactory;
+
+var factory = SplitFactory({
+    core: {
+        authorizationKey: process.env.SPLIT_API_KEY
+    }
+});
+var splitClient = factory.client();
+var treatment = 'control';
+
+splitClient.on(splitClient.Event.SDK_READY, function () {
+    console.log('split.io sdk is ready');
+    treatment = splitClient.getTreatment(process.env.API_KEY, 'hello-treatment');
+    console.log(treatment);
+});
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    if (treatment == 'on') {
+        res.send('Hello, Your Treatment is ON!');
+    } else if (treatment == 'off') {
+        res.send('Hello, Your Treatment is OFF!!');
+    } else {
+        res.send('Hello, This Is The Default ');
+    }
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!'); });
+
 module.exports = app
